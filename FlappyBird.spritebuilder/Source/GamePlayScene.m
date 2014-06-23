4ee44a84ab4246c2621ed0c37c08be7a4630e887
Nina Baculinao <nb2406@columbia.edu>
2014-06-23 13:04:43 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,10 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    // loads a new Character from CCB file created by SpriteBuilder for you named Character
+    [physicsNode addChild:character];
+    // adds character as a child of physicsNode so physics applied to it
 }
 
 -(void)update:(CCTime)delta
@@ -15,5 +19,8 @@
 }
 
 // put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    [character flap];
+}
 
 @end
